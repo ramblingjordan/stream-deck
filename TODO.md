d309b96e00b9c8b3bfceadde6269ac2e49a6ad3b
ramblingjordan <ramblingjordan@gmail.com>
2019-04-02 23:26:06 -0400

    hey it's working!!!

---
+++ TODO.md
@@ -0,0 +1,40 @@
+# TODO
+
+## Requirements
+
+### Setup
+
+- Assign images to keys
+- Connect to OBS
+
+### Runtime
+
+- Switch OBS scenes
+- Count coin finds
+- Trigger stream alerts
+
+## Buttons 15 Total
+
+### Scenes 5
+
+- WIDE MAIN
+- CLOSE HUNT
+- COIN BOX
+- BRB SCREEN
+- LOGO
+
+### Coin Counters 5 
+
+- NIFC
+- Proof
+- 40%
+- 90%
+- Foreign
+
+### Misc
+
+- Start Pre-Show
+- Start Show
+- GAW
+- ???
+- End Show
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import StreamDeck = require('elgato-stream-deck');
-const myStreamDeck = new StreamDeck(); // Will throw an error if no Stream Decks are connected.
+const sd = new StreamDeck(); // Will throw an error if no Stream Decks are connected.
 
-myStreamDeck.on('down', (keyIndex: number) => {
-	console.log('key %d down', keyIndex);
-});
+function startup() {
+  console.debug('Starting Up!')
 
-myStreamDeck.on('up', (keyIndex: number) => {
-	console.log('key %d up', keyIndex);
-});
+  console.debug('Clearing all keys')
+  sd.clearAllKeys()
 
-// Fired whenever an error is detected by the `node-hid` library.
-// Always add a listener for this event! If you don't, errors will be silently dropped.
-myStreamDeck.on('error', (error: string) => {
-	console.error(error);
-});
+  console.log('Maximizing brightness')
+  sd.setBrightness(100)
+
+  console.log('Setting all to green')
+  for(let i=0; i<=14; i++){
+    sd.fillColor(i, 0, 255, 0)
+  }
+}
+
+sd.on('down', (keyIndex: number) => {
+  console.log('key %d down', keyIndex)
+  sd.fillColor(keyIndex, 255, 0, 0)
+})
+
+sd.on('up', (keyIndex: number) => {
+  console.log('key %d up', keyIndex)
+  sd.fillColor(keyIndex, 0, 255, 0)
+})
+
+sd.on('error', (error: string) => {
+	console.error(error)
+})
+
+startup()
