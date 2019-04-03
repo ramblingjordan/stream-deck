70c61d9666f2999dcf0eca11ee81fdbfd1f6556b
ramblingjordan <ramblingjordan@gmail.com>
2019-04-03 00:28:14 -0400

    adding basic obs control

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
-import StreamDeck = require('elgato-stream-deck');
-const sd = new StreamDeck(); // Will throw an error if no Stream Decks are connected.
+import StreamDeck = require('elgato-stream-deck')
+const sd = new StreamDeck()
+import OBSWebSocket = require('obs-websocket-js')
+const obs = new OBSWebSocket()
+
+function setScene(sceneName: string) {
+  obs.send('SetCurrentScene', { "scene-name": sceneName})
+}
+
+function eventHandlers() {
+  sd.on('down', (keyIndex: number) => {
+    console.log('key %d down', keyIndex)
+    sd.fillColor(keyIndex, 255, 0, 0)
+    switch(keyIndex) {
+      case 14: {
+        setScene('Scene - WIDE MAIN')
+      }
+    }
+  })
+  
+  sd.on('up', (keyIndex: number) => {
+    console.log('key %d up', keyIndex)
+    sd.fillColor(keyIndex, 0, 255, 0)
+  })
+  
+  sd.on('error', (error: string) => {
+    console.error(error)
+  }) 
+}
 
 function startup() {
   console.debug('Starting Up!')
@@ -14,20 +41,10 @@ function startup() {
   for(let i=0; i<=14; i++){
     sd.fillColor(i, 0, 255, 0)
   }
+
+  console.log('Connecting to OBS websocket')
+  obs.connect({ address: 'localhost:4444'})
 }
 
-sd.on('down', (keyIndex: number) => {
-  console.log('key %d down', keyIndex)
-  sd.fillColor(keyIndex, 255, 0, 0)
-})
-
-sd.on('up', (keyIndex: number) => {
-  console.log('key %d up', keyIndex)
-  sd.fillColor(keyIndex, 0, 255, 0)
-})
-
-sd.on('error', (error: string) => {
-	console.error(error)
-})
-
 startup()
+eventHandlers()
