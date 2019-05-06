64d74f210bec58395bc822fa0853a69ccab38aa6
ramblingjordan <ramblingjordan@gmail.com>
2019-05-06 08:02:22 -0400

    linted

--- python/main.py
+++ python/main.py
@@ -20,110 +20,111 @@ from StreamDeck.ImageHelpers import PILHelper
 # Generates a custom tile with run-time generated text and custom image via the
 # PIL module.
 def render_key_image(deck, icon_filename, font_filename, label_text):
-    # Create new key image of the correct dimensions, black background
-    image = PILHelper.create_image(deck)
-    draw = ImageDraw.Draw(image)
+  # Create new key image of the correct dimensions, black background
+  image = PILHelper.create_image(deck)
+  draw = ImageDraw.Draw(image)
 
-    # Add image overlay, rescaling the image asset if it is too large to fit
-    # the requested dimensions via a high quality Lanczos scaling algorithm
-    icon = Image.open(icon_filename).convert("RGBA")
-    icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
-    icon_pos = ((image.width - icon.width) // 2, 0)
-    image.paste(icon, icon_pos, icon)
+  # Add image overlay, rescaling the image asset if it is too large to fit
+  # the requested dimensions via a high quality Lanczos scaling algorithm
+  icon = Image.open(icon_filename).convert("RGBA")
+  icon.thumbnail((image.width, image.height - 20), Image.LANCZOS)
+  icon_pos = ((image.width - icon.width) // 2, 0)
+  image.paste(icon, icon_pos, icon)
 
-    # Load a custom TrueType font and use it to overlay the key index, draw key
-    # label onto the image
-    font = ImageFont.truetype(font_filename, 14)
-    label_w, label_h = draw.textsize(label_text, font=font)
-    label_pos = ((image.width - label_w) // 2, image.height - 20)
-    draw.text(label_pos, text=label_text, font=font, fill="white")
+  # Load a custom TrueType font and use it to overlay the key index, draw key
+  # label onto the image
+  font = ImageFont.truetype(font_filename, 14)
+  label_w, label_h = draw.textsize(label_text, font=font)
+  label_pos = ((image.width - label_w) // 2, image.height - 20)
+  draw.text(label_pos, text=label_text, font=font, fill="white")
 
-    return PILHelper.to_native_format(deck, image)
+  return PILHelper.to_native_format(deck, image)
 
 
 # Returns styling information for a key based on its position and state.
 def get_key_style(deck, key, state):
-    # Last button in the example application is the exit button
-    exit_key_index = deck.key_count() - 1
+  # Last button in the example application is the exit button
+  exit_key_index = deck.key_count() - 1
 
-    if key == exit_key_index:
-        name = "exit"
-        icon = "{}.png".format("Exit")
-        font = "Roboto-Regular.ttf"
-        label = "Bye" if state else "Exit"
-    else:
-        name = "emoji"
-        icon = "{}.png".format("Pressed" if state else "Released")
-        font = "Roboto-Regular.ttf"
-        label = "Pressed!" if state else "Key {}".format(key)
+  if key == exit_key_index:
+    name = "exit"
+    icon = "{}.png".format("Exit")
+    font = "Roboto-Regular.ttf"
+    label = "Bye" if state else "Exit"
+  else:
+    name = "emoji"
+    icon = "{}.png".format("Pressed" if state else "Released")
+    font = "Roboto-Regular.ttf"
+    label = "Pressed!" if state else "Key {}".format(key)
 
-    return {
-        "name": name,
-        "icon": os.path.join(os.path.dirname(__file__), "Assets", icon),
-        "font": os.path.join(os.path.dirname(__file__), "Assets", font),
-        "label": label
-    }
+  return {
+    "name": name,
+    "icon": os.path.join(os.path.dirname(__file__), "Assets", icon),
+    "font": os.path.join(os.path.dirname(__file__), "Assets", font),
+    "label": label
+  }
 
 
 # Creates a new key image based on the key index, style and current key state
 # and updates the image on the StreamDeck.
 def update_key_image(deck, key, state):
-    # Determine what icon and label to use on the generated key
-    key_style = get_key_style(deck, key, state)
+  # Determine what icon and label to use on the generated key
+  key_style = get_key_style(deck, key, state)
 
-    # Generate the custom key with the requested image and label
-    image = render_key_image(deck, key_style["icon"], key_style["font"], key_style["label"])
+  # Generate the custom key with the requested image and label
+  image = render_key_image(
+    deck, key_style["icon"], key_style["font"], key_style["label"])
 
-    # Update requested key with the generated image
-    deck.set_key_image(key, image)
+  # Update requested key with the generated image
+  deck.set_key_image(key, image)
 
 
 # Prints key state change information, updates rhe key image and performs any
 # associated actions when a key is pressed.
 def key_change_callback(deck, key, state):
-    # Print new key state
-    print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
+  # Print new key state
+  print("Deck {} Key {} = {}".format(deck.id(), key, state), flush=True)
 
-    # Update the key image based on the new key state
-    update_key_image(deck, key, state)
+  # Update the key image based on the new key state
+  update_key_image(deck, key, state)
 
-    # Check if the key is changing to the pressed state
-    if state:
-        key_style = get_key_style(deck, key, state)
+  # Check if the key is changing to the pressed state
+  if state:
+    key_style = get_key_style(deck, key, state)
 
-        # When an exit button is pressed, close the application
-        if key_style["name"] == "exit":
-            # Reset deck, clearing all button images
-            deck.reset()
+    # When an exit button is pressed, close the application
+    if key_style["name"] == "exit":
+      # Reset deck, clearing all button images
+      deck.reset()
 
-            # Close deck handle, terminating internal worker threads
-            deck.close()
+      # Close deck handle, terminating internal worker threads
+      deck.close()
 
 
 if __name__ == "__main__":
-    streamdecks = DeviceManager().enumerate()
+  streamdecks = DeviceManager().enumerate()
 
-    print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
+  print("Found {} Stream Deck(s).\n".format(len(streamdecks)))
 
-    for index, deck in enumerate(streamdecks):
-        deck.open()
-        deck.reset()
+  for index, deck in enumerate(streamdecks):
+    deck.open()
+    deck.reset()
 
-        # Set initial screen brightness to 30%
-        deck.set_brightness(100)
+    # Set initial screen brightness to 30%
+    deck.set_brightness(30)
 
-        # Set initial key images
-        for key in range(deck.key_count()):
-            update_key_image(deck, key, False)
+    # Set initial key images
+    for key in range(deck.key_count()):
+      update_key_image(deck, key, False)
 
-        # Register callback function for when a key state changes
-        deck.set_key_callback(key_change_callback)
+    # Register callback function for when a key state changes
+    deck.set_key_callback(key_change_callback)
 
-        # Wait until all application threads have terminated (for this example,
-        # this is when all deck handles are closed)
-        for t in threading.enumerate():
-            if t is threading.currentThread():
-                continue
+    # Wait until all application threads have terminated (for this example,
+    # this is when all deck handles are closed)
+    for t in threading.enumerate():
+      if t is threading.currentThread():
+        continue
 
-            if t.is_alive():
-                t.join()
+      if t.is_alive():
+        t.join()
